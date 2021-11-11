c480717f5564208661afc148aac98941e2a96d54
Jade Cormier <cormier.jade@gmail.com>
2021-11-11 14:48:17 +0100

    Back end skeleton ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+image_base_url = 'https://image.tmdb.org/t/p/w200'
+document = JSON.parse(URI.open(url).read)
+document['results'].first(30).each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  rating = movie['vote_average']
+  poster_url = "#{image_base_url}#{movie['poster_path']}"
+  puts "Creating #{title}..."
+  Movie.create(title: title, overview: overview, rating: rating, poster_url: poster_url)
+end
